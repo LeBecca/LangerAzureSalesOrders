2938946c728ba165a3f17a53460d1cbfffcfaa03
Rebecca Lange <LeBecca@users.noreply.github.com>
2018-06-10 22:45:43 -0500

    Things suck less now

--- LangerSalesOrdersWebsite/Controllers/HomeController.cs
+++ LangerSalesOrdersWebsite/Controllers/HomeController.cs
@@ -12,47 +12,43 @@ namespace LangerSalesOrdersWebsite.Controllers
     //[Authorize]
     public class HomeController : Controller
     {
-        //private readonly ISalesOrderRepository _repository;
+        private readonly ISalesOrderRepository _repository;
 
         // TODO: get this from a configuration or let the user select page size
         private const int PAGE_SIZE = 20;
 
-        //public HomeController(ISalesOrderRepository repository)
-        //{
-        //    _repository = repository;
-        //}
-
-        public IActionResult Index()
+        public HomeController(ISalesOrderRepository repository)
         {
-            return View();
+            _repository = repository;
         }
-        //public async Task<IActionResult> Index()
-        //{
-        //    var model = new SalesOrderListModel
-        //    {
-        //        //Items = (await _repository.GetSalesOrdersPagedAsync(1, PAGE_SIZE))
-        //        //    .Select(s => new SalesOrderListItemModel
-        //        //    {
-        //        //        SalesOrderNumber = s.SalesOrderNumber,
-        //        //        DueDate = s.DueDate,
-        //        //        ShipDate = s.ShipDate,
-        //        //        OrderDate = s.OrderDate,
-        //        //        Status = s.Status,
-        //        //        PurchaseOrderNumber = s.PurchaseOrderNumber,
-        //        //        AccountNumber = s.AccountNumber,
-        //        //        SubtotalAmount = s.SubtotalAmount,
-        //        //        TaxAmount = s.TaxAmount,
-        //        //        FreightAmount = s.FreightAmount,
-        //        //        TotalAmountDue = s.TotalAmountDue,
-        //        //        SalesPersonName = string.Join(", ", s.SalesPersonLastName ?? string.Empty, s.SalesPersonFirstName ?? string.Empty)
-        //        //    })
-        //    };
 
-        //    return View(model);
-        //}
+        public async Task<IActionResult> Index()
+        {
+            var model = new SalesOrderListModel
+            {
+                Items = (await _repository.GetSalesOrdersPagedAsync(1, PAGE_SIZE))
+                    .Select(s => new SalesOrderListItemModel
+                    {
+                        SalesOrderNumber = s.SalesOrderNumber,
+                        DueDate = s.DueDate,
+                        ShipDate = s.ShipDate,
+                        OrderDate = s.OrderDate,
+                        Status = s.Status,
+                        PurchaseOrderNumber = s.PurchaseOrderNumber,
+                        AccountNumber = s.AccountNumber,
+                        SubtotalAmount = s.SubtotalAmount,
+                        TaxAmount = s.TaxAmount,
+                        FreightAmount = s.FreightAmount,
+                        TotalAmountDue = s.TotalAmountDue,
+                        SalesPersonName = string.Join(", ", s.SalesPersonLastName ?? string.Empty, s.SalesPersonFirstName ?? string.Empty)
+                    })
+            };
+
+            return View(model);
+        }
 
         [HttpGet]
-        public async Task<IActionResult> Index(int pageNumber)
+        public async Task<IActionResult> ListPage(int pageNumber)
         {
             if (pageNumber <= 0)
             {
